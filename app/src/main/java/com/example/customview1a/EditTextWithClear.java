ad5880c8215358341412a240eee26c4a01ff5fd3
nauvalpradipta <nauvalpradipta2@gmail.com>
2023-05-08 23:48:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/customview1a/EditTextWithClear.java
@@ -0,0 +1,120 @@
+package com.example.customview1a;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.core.content.res.ResourcesCompat;
+
+public class EditTextWithClear extends AppCompatEditText {
+
+    Drawable mClearButtonImage;
+
+    private void init() {
+        // Initialize Drawable member variable.
+        mClearButtonImage =
+                ResourcesCompat.getDrawable(getResources(),
+                        R.drawable.ic_clear_opaque_24dp, null);
+
+        // If the text changes, show or hide the X (clear) button.
+        addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s,
+                                          int start, int count, int after) {
+                // Do nothing.
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s,
+                                      int start, int before, int count) {
+                showClearButton();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Do nothing.
+            }
+        });
+
+        // If the X (clear) button is tapped, clear the text.
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // Use the getCompoundDrawables()[2] expression to check
+                // if the drawable is on the "end" of text [2].
+                if ((getCompoundDrawablesRelative()[2] != null)) {
+                    float clearButtonStart = (getWidth() - getPaddingEnd() - mClearButtonImage.getIntrinsicWidth());
+                    boolean isClearButtonClicked = false;
+                    // Detect the touch in RTL or LTR layout direction.
+                    if (getLayoutDirection() == LAYOUT_DIRECTION_RTL) {
+                        if (event.getX() < clearButtonStart) {
+                            isClearButtonClicked = true;
+                        }
+                    }
+
+                    else {
+                        if (event.getX() > clearButtonStart) {
+                            isClearButtonClicked = true;
+                        }
+                    }
+                    // Check for actions if the button is tapped.
+                    if (isClearButtonClicked) {
+                        // Check for ACTION_DOWN (always occurs before ACTION_UP).
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            // Switch to the black version of clear button.
+                            mClearButtonImage =
+                                    ResourcesCompat.getDrawable(getResources(),
+                                            R.drawable.ic_clear_black_24dp, null);
+                            showClearButton();
+                        }
+                        // Check for ACTION_UP.
+                        if (event.getAction() == MotionEvent.ACTION_UP) {
+                            // Switch to the opaque version of clear button.
+                            mClearButtonImage =
+                                    ResourcesCompat.getDrawable(getResources(),
+                                            R.drawable.ic_clear_opaque_24dp, null);
+                            // Clear the text and hide the clear button.
+                            getText().clear();
+                            hideClearButton();
+                            return true;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                return false;
+            }
+        });
+
+    }
+
+    public EditTextWithClear(@NonNull Context context) {
+        super(context);
+        init();
+    }
+
+    public EditTextWithClear(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public EditTextWithClear(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void showClearButton() {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, mClearButtonImage, null);
+    }
+
+    private void hideClearButton() {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, null, null);
+    }
+}
